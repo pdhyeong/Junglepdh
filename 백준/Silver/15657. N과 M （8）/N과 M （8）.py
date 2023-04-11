fd6c6ebec1ad4beb5ead02cde71b0b19fc11df87
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-04-11 16:38:45 +0900

    [Silver III] Title: N과 M (8), Time: 188 ms, Memory: 117496 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+n,m = map(int,input().split())
+
+lst = list(map(int,input().split()))
+visited = [False] * (n+1)
+
+list = [0] * (n+1)
+
+lst.sort()
+
+def nandm(k,num):
+  if k == m:
+    for i in range(m):
+      print(list[i],end = " ")
+    print()
+    return
+  else:
+    for i in range(num,n):
+      list[k] = lst[i]
+      visited[i] = True
+      nandm(k+1,i)
+      visited[i] = False
+nandm(0,0)
