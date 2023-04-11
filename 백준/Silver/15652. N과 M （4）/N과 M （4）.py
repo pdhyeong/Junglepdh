3c49c441248bfe84b2cd132d66f5b0e78bda0e2e
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-04-11 14:09:46 +0900

    [Silver III] Title: N과 M (4), Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+n,m = map(int,input().split())
+
+list = [0] * (n+1)
+visited = [False] * (n+1)
+
+def nandm(k,num):
+  if k == m:
+    for i in range(m):
+      print(list[i],end=" ")
+    print()
+    return
+  else:
+    for i in range(num,n+1):
+      list[k] = i
+      visited[i] = True
+      nandm(k+1,i)
+      visited[i] = False
+nandm(0,1)
