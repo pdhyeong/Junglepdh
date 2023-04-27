95d0949601e7822a1cc724c5c8071d150d9221dd
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-04-27 23:26:53 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import chain
+from collections import deque
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (n+1)
+dp[1] = 1
+
+for i in range(2,n+1):
+  dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
