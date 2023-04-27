02f4d1f88cd9656f961ed5b12036204f70e62659
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-04-27 10:42:34 +0900

    [Silver IV] Title: 바닥 장식, Time: 100 ms, Memory: 34480 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,36 @@
+import sys
+from itertools import chain
+from collections import deque
+import heapq
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+house = []
+search = [1,-1]
+for i in range(n):
+  house.append(list(input().rstrip()))
+
+def dfs(x,y):
+    if house[x][y] == '-':
+        house[x][y] = 1	    
+        for i in range(2):   
+            ny = y + search[i]
+            if 0<= ny < m and house[x][ny] == '-':
+                dfs(x,ny)
+    if house[x][y] == '|':
+        house[x][y] = 1	    
+        for i in range(2):   
+            nx = x + search[i]
+            if 0<= nx < n and house[nx][y] == '|':
+                dfs(nx,y)
+
+
+answer = 0
+for i in range(n):
+    for j in range(m):
+        if house[i][j] == '-' or house[i][j] == '|':
+            dfs(i,j)    # 노드가 '-'이나 '|'일 경우에 재귀함수 호출
+            answer += 1
+
+print(answer)
