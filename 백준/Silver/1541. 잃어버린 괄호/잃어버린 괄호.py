dffdabf13f63cb55a00e1cf2e3e4cbed49831ea0
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-05-02 15:33:42 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -5,11 +5,15 @@ st = input().rstrip()
 
 se = st.split('-')
 for i in range(len(se)):
-  newlst = se[i].split('+')
-  sm = 0
-  for st in newlst:
-      sm += int(st)
-  se[i] = sm
+  if '+' in se[i]:
+    newlst = se[i].split('+')
+    sm = 0
+    for st in newlst:
+        sm += int(st)
+    se[i] = sm
+  else:
+    se[i] = int(se[i])
+
 result = se[0]
 for i in range(1,len(se)):
   result -= se[i]
