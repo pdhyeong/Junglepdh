426ae76678252cf79192b038deb107acc20ebbe7
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-05-19 01:10:05 +0900

    [Silver I] Title: 정수 삼각형, Time: 148 ms, Memory: 38616 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,14 +1,22 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
 n = int(input())
-hr = [[0] * (n+1) for i in range(n)]
 
+tri = []
 for i in range(n):
-    line = list(map(int, input().split()))
-    for j in range(i+1):
-        hr[i][j] = line[j]
+  line = list(map(int,input().split()))
+  tri.append(line + ([0] * (n-(i+1))))
 
-for i in range(1, n):
-    for j in range(i+1):
-        hr[i][j] += max(hr[i-1][j-1] if j > 0 else 0, hr[i-1][j])
+for i in range(1,n):
+  for j in range(i+1):
+    if j == 0:
+      tri[i][j] = tri[i][j] + tri[i-1][j]
+    elif j == i:
+      tri[i][j] = tri[i][j] + tri[i-1][j-1]
+    else:
+      tri[i][j] = tri[i][j] + max(tri[i-1][j-1], tri[i-1][j])
 
-result = max(hr[n-1])
-print(result)
+print(max(tri[n-1]))
