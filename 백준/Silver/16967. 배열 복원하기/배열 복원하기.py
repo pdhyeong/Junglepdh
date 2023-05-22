806f9d706c1d0d04b98dc2a32d49574fcf2b82ea
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-05-22 12:00:37 +0900

    [Silver III] Title: 배열 복원하기, Time: 196 ms, Memory: 119448 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -7,11 +7,17 @@ for i in range(h+x):
   k = list(map(int,input().split()))
   lst.append(k)
 
+nelst = [[0] * (w) for _ in range(h)]
+
 for i in range(x,h+x):
   for j in range(y,w+y):
     lst[i][j] -= lst[i-x][j-y]
+    
+for i in range(h):
+  for j in range(w):
+    nelst[i][j] = lst[i][j]
 
 for i in range(h):
   for j in range(w):
-    print(lst[i][j],end=' ')
+    print(nelst[i][j],end=' ')
   print()
