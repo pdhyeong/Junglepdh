e20f7d0318a352df34241677b95932af485de2bd
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-08-21 22:27:00 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,34 @@
+function solution(X, Y) {    
+    let answer = [];
+    let stack = [];
+    
+    for(let i = 0; i < X.length; i++){
+        if(stack.includes(X[i])){
+            continue;
+        }
+        
+        const yArr = Y.split(X[i]);
+        
+        if(yArr.length !== 1){
+            const xArr = X.split(X[i]);
+            
+            const min = Math.min(xArr.length - 1, yArr.length - 1);
+            
+            answer.push(X[i].repeat(min));
+        }
+        
+        stack.push(X[i]);
+    }
+    
+    if(answer.length === 0){
+        return "-1";
+    }
+    
+    if(Number(answer.join("")) === 0){
+        return "0";
+    }
+    
+    answer = answer.join("").split("").sort((a,b) => b - a);
+    
+    return answer.join("");
+}
