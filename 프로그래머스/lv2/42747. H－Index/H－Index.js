bd099ce52def6b14c5c23f912bdf941d0fa6e677
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-08-28 16:05:17 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,10 +1,6 @@
 function solution(citations) {
     var answer = 0;
     citations.sort((a,b) => b-a);
-    citations.forEach((e,index) => {
-        if(index + 1 <= e){
-            answer = index+1;
-        }
-    })
+    citations.forEach((e,index) => {if(index + 1 <= e && e >= citations.length - (index+1)){answer = index+1;}});
     return answer;
 }
