248e7516a14b72927657c93ae88722c7d2ab1e2c
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-08-30 05:06:56 +0900

    [level 1] Title: 실패율, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -0,0 +1,21 @@
+function solution(N, stages) {
+    var answer = [];
+    var remain = stages.length;
+    var sl = [];
+    stages.sort((a,b) => a-b);
+    for(let i = 1;i<=N;i++){
+        var count = 0;
+        for(let j = 0;j<stages.length;j++){
+            if(i === stages[j]){
+                count += 1;
+            }
+        }
+        sl.push([count/remain,i]);
+        remain -= count;
+    }
+    sl.sort((a,b) => b[0] - a[0]);
+    sl.forEach(e => {
+        answer.push(e[1]);
+    })
+    return answer;
+}
