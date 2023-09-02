f44e713d3756978c0b231436c62244b61a8d4fe3
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-02 23:32:07 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -0,0 +1,24 @@
+function solution(sequence, k) {
+    let answer = [];
+    let sum = 0;
+    let trace = 0;
+    let interval = sequence.length;
+    for(let i = 0 ;i<sequence.length;i++){
+        sum += sequence[i];
+        while(sum > k){
+            sum -= sequence[trace++];
+        }
+        if(sum === k){
+            answer.push([trace,i]);
+        }
+    }
+    let min = sequence.length
+    let savevalue = 0;
+    for(let i = 0;i<answer.length;i++){
+        if(answer[i][1] - answer[i][0] < min){
+            min = answer[i][1] - answer[i][0];
+            savevalue = i;
+        }
+    }
+    return answer[savevalue];
+}
