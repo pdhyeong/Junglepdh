b77c844cb37a63417b77061474e7b045bef99b68
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-05 01:13:33 +0900

    [level 1] Title: 소수 찾기, Time: 8.44 ms, Memory: 7.36 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.cpp
@@ -7,10 +7,12 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
     int arr[1000001]={0,};
-    
-    for(int i= 2;i<=n;i++){
-        for(int j = 2;i*j<=n;j++){
-            arr[i*j] = 1;
+    int i,j;
+    for(i= 2;i<=n;i++){
+        if(arr[i] == 0){
+            for(j = 2;i*j<=n;j++){
+               arr[i*j] = 1;
+            }
         }
     }
     for(int i = 2;i<=n;i++){
