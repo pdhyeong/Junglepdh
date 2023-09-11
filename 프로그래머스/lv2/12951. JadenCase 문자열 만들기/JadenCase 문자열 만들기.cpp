348fce8b6ee6176d7d1fb8168b6351dce4fa5a84
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-11 22:31:36 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    string su = "";
+    vector<string> st;
+    for(int i = 0;i<s.size();i++){
+        su += s[i];
+        if(s[i] == ' '){
+            st.push_back(su);
+            su = "";
+        }
+    }
+    st.push_back(su);
+    for(int i = 0;i<st.size();i++){
+        if(st[i][0] >= 'a' && st[i][0] <= 'z'){
+            st[i][0] -= 32;
+        }
+        for(int j = 1;j<st[i].size();j++){
+            if(st[i][j] >= 'A' && st[i][j] <= 'Z'){
+                st[i][j] += 32;
+            }
+            cout << st[i][j] << ' ';
+        }
+        answer += st[i];
+    }
+    return answer;
+}
