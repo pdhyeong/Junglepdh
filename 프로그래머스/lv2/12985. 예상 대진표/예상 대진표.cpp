5b9c9e82d32d2533b29a6ecde00b282b9e7f742b
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-15 02:13:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int solution(int n, int a, int b)
 {
     int answer = 0;
+    
+    // [{(1,2) , (3,4)} , {(5,6) , (7,8)}]
     while(a != b){
         a = (a + 1) >> 1;
         b = (b + 1) >> 1;
