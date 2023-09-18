ebcbeb27839c6bb40c184aae94b3276c21565b55
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-18 11:11:13 +0900

    [level 2] Title: 더 맵게, Time: 222.65 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -1,19 +1,25 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include <queue>
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-     int answer = 0;
-    priority_queue<int, vector<int>, greater<int> > pq(scoville.begin(), scoville.end());
-    while (pq.size() > 1 && pq.top() < K) {
-        int first = pq.top();
+    int answer = 0;
+    priority_queue <int, vector<int>, greater<int> > pq;
+    for(int i = 0;i<scoville.size();i++){
+        pq.push(scoville[i]);
+    }
+    while(pq.top() < K && pq.size() > 1){
+        int s1 = pq.top();
         pq.pop();
-        int second = pq.top();
+        int s2 = pq.top();
         pq.pop();
-        pq.push(first + (second * 2));
+        pq.push(s1 + s2*2);
         answer++;
     }
-    if (pq.top() < K)    return -1;
+    if(pq.top() < K){
+        return -1;
+    }
     return answer;
 }
