5ef3c4163fef3e0aed61c2d59883128e32236e64
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-18 11:25:40 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.28 ms, Memory: 3.87 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.cpp
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int solution(int n) {
     int arr[60001] = {0,};
+    arr[0] = 1;
     arr[1] = 1;
-    arr[2] = 2;
-    for (int i = 3; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
         arr[i] = (arr[i - 1] + arr[i - 2]) % 1000000007;
     }
     return arr[n];
