3f797f206679c14f0fe2930f5e33ee72c46d8b73
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-20 19:03:44 +0900

    [level 3] Title: 단속카메라, Time: 0.59 ms, Memory: 3.99 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int solution(vector<vector<int>> routes) {
     int answer = 0;
     sort(routes.begin(),routes.end());
-    int install = routes[0][0] - 1;
+    int install = routes[0][0]-1;
     for(int i = 0;i<routes.size();i++){
         int in = routes[i][0];
         int out = routes[i][1];
