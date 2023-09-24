f8cdceff11f931cc608088fbf5e90b9475826edc
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-09-24 16:12:54 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    char tmp;
+    for(int i = 0;i<my_string.size()/2;i++){
+        tmp = my_string[i];
+        my_string[i] = my_string[my_string.size()-i-1];
+        my_string[my_string.size()-i-1] = tmp;
+    }
+    return my_string;
+}
