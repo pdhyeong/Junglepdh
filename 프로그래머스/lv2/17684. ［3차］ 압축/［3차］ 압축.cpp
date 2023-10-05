f28e58b81c5d09efb1dcde1fff388b77b04d1cb7
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-10-05 17:25:32 +0900

    [level 2] Title: [3차] 압축, Time: 0.33 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(string msg) {
+    vector<int> answer;
+    map<string,int> dict;
+    int num = 1;
+    for(char i = 'A';i<='Z';i++){
+        string a = "";
+        a += i;
+        dict[a] = num++;
+    }
+    string search = "";
+    int max_size = 27;
+    for (int i = 0; i < msg.length(); i++)
+    {
+        search += msg[i];
+        if (dict[search] == 0)
+        {
+            dict[search] = max_size++;
+            search = search.substr(0, search.length() - 1);
+            answer.push_back(dict[search]);
+            search = "";
+            i--;
+        }
+    }
+    answer.push_back(dict[search]);
+    return answer;
+}
