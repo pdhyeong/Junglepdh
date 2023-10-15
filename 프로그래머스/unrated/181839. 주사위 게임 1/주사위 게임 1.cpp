e752946515753c8304308880e925237b04002afd
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-10-16 02:37:29 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int solution(int a, int b) {
+    if((a & 1) && (b & 1)){
+        return a * a + b * b;
+    }
+    else if((a & 1) || (b & 1)){
+        return 2 * (a + b);
+    }
+    else{
+        return abs(a - b);
+    }
+}
