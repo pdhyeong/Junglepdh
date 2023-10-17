addc21651b7c39a7d1b82cd77bf9b23b0f1a44ea
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-10-17 23:35:34 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628#) 
 
 ### 성능 요약
 
-메모리: 3.7 MB, 시간: 0.02 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 22:22:19
+2023년 10월 2일 23:35:32
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,48 +1,36 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <queue>
+#include <algorithm>
+#include <deque>
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
     vector<int> answer;
-    priority_queue<int> max_pq;
-    priority_queue<int,vector<int>,greater<int>> min_pq;
-    
+    deque<int> dq;
     for(int i = 0;i<operations.size();i++){
         char check = operations[i][0];
-        cout << check << ' ';
         string s = operations[i].substr(2);
         if(check == 'I'){
-            max_pq.push(stoi(s));
-            min_pq.push(stoi(s));
+            dq.push_back(stoi(s));
+            sort(dq.begin(),dq.end());
         }
         else if(check == 'D' && s[0] == '-'){
-            if(min_pq.empty()){
-                continue;
-            }
-            if(min_pq.top() == max_pq.top()) max_pq.pop();
-            min_pq.pop();
+            if(dq.size() == 0) continue;
+            dq.pop_front();
         }
         else if(check == 'D' && stoi(s)){
-            if(max_pq.empty()){
-                continue;
-            }
-            if(min_pq.top() == max_pq.top()) min_pq.pop();
-            max_pq.pop();
-        }
-        if(min_pq.top() > max_pq.top()){
-            while(!min_pq.empty()) min_pq.pop();
-            while(!max_pq.empty()) max_pq.pop();
+            if(dq.size() == 0) continue;
+            dq.pop_back();
         }
     }
-    if(min_pq.empty()){
+    if(dq.size() == 0){
         answer.push_back(0);
         answer.push_back(0);
     }
     else{
-        answer.push_back(max_pq.top());
-        answer.push_back(min_pq.top());
+        answer.push_back(dq.back());
+        answer.push_back(dq.front());
     }
     return answer;
 }
