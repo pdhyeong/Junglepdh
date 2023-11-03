5e43cee93675a4d8d691cec5b47fa70d45d6ae9f
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-11-04 03:25:11 +0900

    [level 3] Title: 야근 지수, Time: 16.52 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
@@ -2,19 +2,21 @@
 #include <vector>
 #include <queue>
 using namespace std;
+
 long long solution(int n, vector<int> works) {
     long long answer = 0;
-    priority_queue <int> q; 
-    int number; 
-    for(auto x : works)q.push(x); 
-    while(n--&&!q.empty()){
-        number = q.top();
-        q.pop(); 
-        if(number!=1)q.push(number-1);
+    priority_queue<int> pq;
+    for(int i = 0;i<works.size();i++) pq.push(works[i]);
+    while(n-- && !pq.empty()){
+        if(pq.top() < 1){
+            return 0;
+        }
+        pq.push(pq.top()-1);
+        pq.pop();
     }
-    while(!q.empty()){
-        answer+=(q.top()*q.top()); 
-        q.pop(); 
-    }        
+    while(!pq.empty()){
+        answer += pq.top() * pq.top();
+        pq.pop();
+    }
     return answer;
 }
