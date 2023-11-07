5a3ef263ce98225969001f2c1d0eb3af3aa152e0
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-11-07 14:54:48 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -7,11 +7,15 @@ int solution(int n) {
     int answer = 0;
     for(int i = 1;i<=n;i++){
         int sum = 0;
-        for(int j = i;sum<n;j++){
+        for(int j = i;j<=n;j++){
             sum += j;
-        }
-        if(sum == n){
-            answer++;
+            if(sum == n){
+                answer++;
+                break;
+            }
+            if(sum > n){
+                break;
+            }
         }
     }
     return answer;
