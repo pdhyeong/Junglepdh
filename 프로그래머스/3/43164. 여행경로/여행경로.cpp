28f3cea6f2f9cc28488ba019a2b487c250f054e7
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-11-09 13:52:35 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<string> answer;
+bool visited[100000001];
+bool flag = false;
+int cnt = 0;
+
+void dfs(string cur, vector<vector<string>> tickets){
+    if(cnt == (int)tickets.size()) flag = true;
+    answer.push_back(cur);
+    for(int i=0; i<tickets.size(); i++){
+        if(!visited[i] && tickets[i][0] == cur){
+            visited[i] = true;
+            cnt++;
+            dfs(tickets[i][1], tickets);
+
+            if(!flag){
+                answer.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+}
+vector<string> solution(vector<vector<string>> tickets) {
+    sort(tickets.begin(),tickets.end());
+    dfs("ICN",tickets);
+    return answer;
+}
