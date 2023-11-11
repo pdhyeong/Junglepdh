bbe90ece9f19fe909412e5619cf2af4ba18cb5c5
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-11-11 19:38:12 +0900

    [Gold V] Title: 두 용액, Time: 68 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cc
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -4,20 +4,9 @@
 #include <algorithm>
 using namespace std;
 
-void swap(int *a,int *b){
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-void check(int a,int b){
-    if(a > b){
-        swap(&a,&b);
-    }
-}
 int main() {
     int n;
-    vector<long long> arr;
+    vector<int> arr;
     cin >> n;
     for(int i = 0;i<n;i++){
         int k;
@@ -29,25 +18,27 @@ int main() {
     int start = 0;
     int end = n-1;
     long long sum = 10000000001;
-    long long save[2] = {0,0};
+    vector<int> save(2);
     while(start < end){
         long long result = arr[start] + arr[end];
         if(sum > abs(result)){
             sum = abs(result);
             save[0] = arr[start];
             save[1] = arr[end];
-            if(result == 0){
-                break;
-            }
         }
         if(result < 0){
             start++;
+        }
+        else if(result == 0){
+            save[0] = arr[start];
+            save[1] = arr[end];
+            break;
         }
         else{
             end--;
         }
     }
-    check(save[0],save[1]);
+    sort(save.begin(),save.end());
     cout << save[0] << ' ' << save[1];
     return 0;
 }
