90fa80398458aa713ab3ec632e5cbbbb81d12ce0
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-11-20 19:42:25 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.rbegin(),citations.rend());
+    int s = citations.size();
+    for(int i = 0;i<citations.size();i++){
+        if(citations[i] < i+1){
+            return i;
+        }
+    }
+    return citations.size();
+}
