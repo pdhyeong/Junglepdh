de06ee840478810b0e95802dea0852ad7d9d87f3
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-01-02 16:02:46 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
-int memo[1001];
-int recur(int n,int cn){
-    if(n == 0){
-        return cn;
-    }
-    if(n % 2 == 0){
-        return recur(n / 2,cn);
-    }
-    return recur(n-1,cn+1);
-}
+
 int solution(int n)
 {
-    int ans = recur(n,0);
+    int ans = 0;
+    while(n > 0){
+        if(n & 1){
+            ans++;
+            n--;
+        }
+        else{
+            n /= 2;
+        }
+    }
     return ans;
 }
