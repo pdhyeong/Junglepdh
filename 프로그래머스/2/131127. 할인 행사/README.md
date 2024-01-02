b2b9881a72288fb04810f7603e646854d1f50b76
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-01-02 16:42:05 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 3:51:26
+2024년 1월 2일 16:42:3
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,29 +1,32 @@
 #include <string>
 #include <vector>
-#include <set>
-
+#include <unordered_map>
+#include <iostream>
 using namespace std;
 
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    multiset<string> ms, tmp;
     int answer = 0;
-
-    for(int i = 0; i < want.size(); i++) {
-        for(int j = 0; j < number[i]; j++) {
-            ms.insert(want[i]);
-        }
+    unordered_map<string,int> mymap;
+    unordered_map<string,int> mymap2;
+    
+    for(int i = 0;i<want.size();i++){
+        mymap[want[i]] = number[i];
     }
-
-    for(int i = 0; i < discount.size() - 9; i++) {
-        tmp = ms;
-        for(int j = 0; j < 10; j++) {
-            multiset<string>::iterator it = tmp.find(discount[i+j]);
-            if(it != tmp.end()) {
-                tmp.erase(it);
+    for(int i = 0;i<discount.size()-9;i++){
+        mymap2 = mymap;
+        bool flag = false;
+        for(int j = i;j<i+10;j++){
+            mymap2[discount[j]]--;
+        }
+        for(auto &pair:mymap2){
+            if(pair.second != 0){
+                flag = true;
+                break;
             }
         }
-        if(tmp.empty()) answer++;
+        if(!flag){
+            answer++;
+        }
     }
-
     return answer;
 }
