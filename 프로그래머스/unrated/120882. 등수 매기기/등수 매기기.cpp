5d2b6e600aaabb6ac30e45d02fa62bfdedbfa159
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-01-06 04:16:43 +0900

    [level unrated] Title: 등수 매기기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.cpp
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.cpp
@@ -4,12 +4,9 @@
 using namespace std;
 vector<int> solution(vector<vector<int>> score) {
     vector<int> answer(score.size(),1);
-    vector<double> score_list;
+    vector<int> score_list;
     for(int i = 0;i<score.size();i++){
-        double en = score[i][0];
-        double math_score = score[i][1];
-        double avg = (en + math_score) / 2;
-        score_list.push_back(avg);
+        score_list.push_back(score[i][0]+score[i][1]);
     }
     for(int i = 0;i<score_list.size();i++){
         for(int j = 0;j<score_list.size();j++){
