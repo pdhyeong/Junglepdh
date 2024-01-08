66d07b480407ea17e3d6ca84faedda3d2d821847
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-01-09 00:07:43 +0900

    [level 2] Title: 귤 고르기, Time: 13.43 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,25 +1,25 @@
 #include <string>
 #include <vector>
-#include <map>
-#include <algorithm>
+#include <unordered_map>
 #include <iostream>
+#include <algorithm>
 using namespace std;
-bool comp(pair<int,int> le,pair<int,int> ri){
-    return le.second > ri.second;
+bool comp(pair<int,int> a,pair<int,int> b){
+    return a.first > b.first;
 }
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
-    map<int,int> mymap;
-    vector<pair<int,int>> vec;
+    unordered_map<int,int> mymap;
+    vector<pair<int,int>> now;
     for(int i = 0;i<tangerine.size();i++){
         mymap[tangerine[i]]++;
     }
-    for(const auto & pair : mymap){
-        vec.push_back({pair.first,pair.second});
+    for(auto & pair:mymap){
+        now.push_back({pair.second,pair.first});
     }
-    sort(vec.begin(),vec.end(),comp);
-    for(int i = 0;i<vec.size();i++){
-        k -= vec[i].second;
+    sort(now.begin(),now.end(),comp);
+    for(int i = 0;i<now.size();i++){
+        k -= now[i].first;
         answer++;
         if(k <= 0){
             break;
