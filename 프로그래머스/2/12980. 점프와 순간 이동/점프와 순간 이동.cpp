874bef825132978acb32ddedbeb9b891f5e6e2a4
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-01-08 23:36:45 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.48 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -5,13 +5,8 @@ int solution(int n)
 {
     int ans = 0;
     while(n > 0){
-        if(n & 1){
-            ans++;
-            n--;
-        }
-        else{
-            n /= 2;
-        }
+        ans += n%2;
+        n /= 2;
     }
     return ans;
 }
