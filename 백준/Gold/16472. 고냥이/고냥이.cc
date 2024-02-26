c1e73b2e5bb19a8e5eac68caa975363e24fb5134
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-02-26 19:34:56 +0900

    [Gold IV] Title: 고냥이, Time: 12 ms, Memory: 2272 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.cc
+++ 백준/Gold/16472. 고냥이/고냥이.cc
@@ -11,13 +11,11 @@ int main(){
     cin >> s;
 
     int start = 0;
-    int end = 1;
     int max_tor = 0;
     unordered_map<char,int> al;
     al[s[start]]++;
     for(int i = 1;i<s.size();i++){
         al[s[i]]++;
-        end = i;
         while(al.size() > torelnce){
             al[s[start]]--;
             if(al[s[start]] == 0){
@@ -25,8 +23,8 @@ int main(){
             }
             start++;
         }
-        if(max_tor < end - start + 1){
-            max_tor = end - start + 1;
+        if(max_tor < i - start + 1){
+            max_tor = i - start + 1;
         }
     }
     cout << max_tor;
