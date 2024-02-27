d003cb6fa0e5eeebda8b2b26b23f5a3a5b34e587
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-02-27 17:13:32 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,29 +1,36 @@
 #include <string>
 #include <vector>
-#include <set>
-
+#include <unordered_map>
+#include <iostream>
 using namespace std;
 
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    multiset<string> ms, tmp;
     int answer = 0;
-
-    for(int i = 0; i < want.size(); i++) {
-        for(int j = 0; j < number[i]; j++) {
-            ms.insert(want[i]);
-        }
+    unordered_map<string,int> mymap;
+    int num = 0;
+    for(int i = 0;i<want.size();i++){
+        mymap[want[i]] = number[i];
+        num += number[i];
     }
-
-    for(int i = 0; i < discount.size() - 9; i++) {
-        tmp = ms;
-        for(int j = 0; j < 10; j++) {
-            multiset<string>::iterator it = tmp.find(discount[i+j]);
-            if(it != tmp.end()) {
-                tmp.erase(it);
+    for(int i = 0;i<discount.size()-9;i++){
+        unordered_map<string,int> comp_map = mymap;
+        bool flag = false;
+        for(int j = i;j<i+10;j++){
+            if(comp_map.find(discount[j]) != comp_map.end()){
+                comp_map[discount[j]]--;
+                if(comp_map[discount[j]] < 0){
+                    flag = true;
+                    break;
+                }
+            }
+            else{
+                flag = true;
+                break;
             }
         }
-        if(tmp.empty()) answer++;
+        if(!flag){
+            answer++;
+        }
     }
-
     return answer;
 }
