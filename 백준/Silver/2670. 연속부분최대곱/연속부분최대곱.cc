8cd07e732a5746b19c6b17edb481873e4a06c1b3
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-04-03 13:19:52 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 84 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+#define MAX(a,b) (((a) > (b) ? (a) : (b)))
+int main() {
+    int n;
+    cin >> n;
+
+    vector<double> vec;
+
+    for(int i = 0;i<n;i++){
+        double f;
+        cin >> f;
+        vec.push_back(f);
+    }
+
+    double max_value = 0;
+    for(int i = 0;i<vec.size();i++){
+        double comp_value = 1;
+        for(int j = i;j<vec.size();j++){
+            comp_value *= vec[j];
+            max_value = MAX(comp_value,max_value);
+        }
+    }
+    printf("%.3lf",max_value);
+    return 0;
+}
