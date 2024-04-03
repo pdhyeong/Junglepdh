4221dc2dd3341fc7be040559bee20f375f76c825
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-04-03 13:34:13 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -10,20 +10,27 @@ int main() {
     cin >> n;
 
     vector<double> vec;
-
+    double dp[10001];
     for(int i = 0;i<n;i++){
         double f;
         cin >> f;
         vec.push_back(f);
     }
 
-    double max_value = 0;
-    for(int i = 0;i<vec.size();i++){
-        double comp_value = 1;
-        for(int j = i;j<vec.size();j++){
-            comp_value *= vec[j];
-            max_value = MAX(comp_value,max_value);
-        }
+    // double max_value = 0;
+    // for(int i = 0;i<vec.size();i++){
+    //     double comp_value = 1;
+    //     for(int j = i;j<vec.size();j++){
+    //         comp_value *= vec[j];
+    //         max_value = MAX(comp_value,max_value);
+    //     }
+    // }
+    // printf("%.3lf",max_value);
+    dp[0] = vec[0];
+    double max_value = -1000;
+    for(int i = 1;i<vec.size();i++){
+        dp[i] = MAX(vec[i],dp[i-1] * vec[i]);
+        max_value = MAX(max_value,dp[i]);
     }
     printf("%.3lf",max_value);
     return 0;
