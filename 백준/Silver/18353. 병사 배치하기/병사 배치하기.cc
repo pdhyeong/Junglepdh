2497c7c0d512ba264c3e18b629af7dc4ca377206
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-08-25 17:05:54 +0900

    [Silver II] Title: 병사 배치하기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
@@ -21,19 +21,16 @@ int main(void) {
 
     // 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
     int dp[2000]; 
-    for (int i = 0; i < n; i++) {
-        dp[i] = 1;
-    }
-
+    dp[0] = 1;
     // 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
     for (int i = 1; i < n; i++) {
+        dp[i] = 1;
         for (int j = 0; j < i; j++) {
             if (v[j] < v[i]) {
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         }
     }
-    
     // 열외해야 하는 병사의 최소 수를 출력
     int maxValue = 0;
     for (int i = 0; i < n; i++) {
