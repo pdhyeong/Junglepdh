8af5cb3f607cb6d80472bb812d8578830b88c9a6
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-09-01 16:36:44 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 36.54 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+long long solv(long long num) {
+
+    if (num % 2 == 0) return num + 1;
+
+    long long i = 1;
+    while (((num + 1) & (1L << i)) == 0)
+        i++;
+
+    num |= (1L << i);
+    num -= (1L << (i - 1));
+    return num;
+}
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    
+    for(int i = 0;i<numbers.size();i++){
+        answer.push_back(solv(numbers[i]));
+    }
+    return answer;
+}
