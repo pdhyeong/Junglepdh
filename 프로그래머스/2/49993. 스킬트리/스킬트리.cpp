f8eb4b3109edaa03d032bcfe4c917c1d334c5639
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-12-31 20:53:05 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    int count = 0;
+    for(int i = 0;i<skill_trees.size();i++){
+        int index = 0;
+        string temp = "";
+        for(int j = 0;j<skill_trees[i].size();j++){
+            for(int k = 0;k<skill.size();k++){
+                if(skill_trees[i][j] == skill[k]){
+                    temp += skill[k];
+                    break;
+                }
+            }
+        }
+        for(int comp_index = 0;comp_index<temp.size();comp_index++){
+            if(temp[comp_index] != skill[comp_index]){
+                cout << skill[comp_index] << ' ';
+                count++;
+                break;
+            }
+        }
+    }
+    return skill_trees.size()-count;
+}
